93e60a25b5afab85dd185c3ddd87868c8de54027
GK-2904 <kambleganesh747@gmail.com>
2026-02-17 23:29:12 +0530

    Manually parse DB URL to enforce IPv4

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -12,24 +12,33 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ...(process.env.DATABASE_URL
-    ? {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-      family: 4,
-    }
-    : {
-      user: process.env.DB_USER,
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
-      port: parseInt(process.env.DB_PORT || '5432'),
-      family: 4,
-    }),
-});
+let poolConfig;
+
+if (process.env.DATABASE_URL) {
+  const params = new URL(process.env.DATABASE_URL);
+  poolConfig = {
+    user: params.username,
+    password: params.password,
+    host: params.hostname,
+    port: parseInt(params.port),
+    database: params.pathname.split('/')[1],
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    family: 4, // Explicitly force IPv4
+  };
+} else {
+  poolConfig = {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || '5432'),
+    family: 4,
+  };
+}
+
+const pool = new Pool(poolConfig);
 
 export const query = (text: string, params?: any[]) => pool.query(text, params);
 
